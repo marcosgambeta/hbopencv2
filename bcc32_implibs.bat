58ecfd4caa374b89f3868d709b1d427124d5e441
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-03-24 11:32:10 -0300

    Add bcc32_implibs.bat

---
+++ bcc32_implibs.bat
@@ -0,0 +1,40 @@
+rem libraries
+implib -a lib\opencv_calib3d2413     %OPENCV2_BIN_DIR%\opencv_calib3d2413.dll
+implib -a lib\opencv_contrib2413     %OPENCV2_BIN_DIR%\opencv_contrib2413.dll
+implib -a lib\opencv_core2413        %OPENCV2_BIN_DIR%\opencv_core2413.dll
+implib -a lib\opencv_features2d2413  %OPENCV2_BIN_DIR%\opencv_features2d2413.dll
+implib -a lib\opencv_ffmpeg2413      %OPENCV2_BIN_DIR%\opencv_ffmpeg2413.dll
+implib -a lib\opencv_flann2413       %OPENCV2_BIN_DIR%\opencv_flann2413.dll
+implib -a lib\opencv_gpu2413         %OPENCV2_BIN_DIR%\opencv_gpu2413.dll
+implib -a lib\opencv_highgui2413     %OPENCV2_BIN_DIR%\opencv_highgui2413.dll
+implib -a lib\opencv_imgproc2413     %OPENCV2_BIN_DIR%\opencv_imgproc2413.dll
+implib -a lib\opencv_legacy2413      %OPENCV2_BIN_DIR%\opencv_legacy2413.dll
+implib -a lib\opencv_ml2413          %OPENCV2_BIN_DIR%\opencv_ml2413.dll
+implib -a lib\opencv_nonfree2413     %OPENCV2_BIN_DIR%\opencv_nonfree2413.dll
+implib -a lib\opencv_objdetect2413   %OPENCV2_BIN_DIR%\opencv_objdetect2413.dll
+implib -a lib\opencv_ocl2413         %OPENCV2_BIN_DIR%\opencv_ocl2413.dll
+implib -a lib\opencv_photo2413       %OPENCV2_BIN_DIR%\opencv_photo2413.dll
+implib -a lib\opencv_stitching2413   %OPENCV2_BIN_DIR%\opencv_stitching2413.dll
+implib -a lib\opencv_superres2413    %OPENCV2_BIN_DIR%\opencv_superres2413.dll
+implib -a lib\opencv_video2413       %OPENCV2_BIN_DIR%\opencv_video2413.dll
+implib -a lib\opencv_videostab2413   %OPENCV2_BIN_DIR%\opencv_videostab2413.dll
+
+rem debug libraries
+rem implib -a lib\opencv_calib3d2413d    %OPENCV2_BIN_DIR%\opencv_calib3d2413d.dll
+rem implib -a lib\opencv_contrib2413d    %OPENCV2_BIN_DIR%\opencv_contrib2413d.dll
+rem implib -a lib\opencv_core2413d       %OPENCV2_BIN_DIR%\opencv_core2413d.dll
+rem implib -a lib\opencv_features2d2413d %OPENCV2_BIN_DIR%\opencv_features2d2413d.dll
+rem implib -a lib\opencv_flann2413d      %OPENCV2_BIN_DIR%\opencv_flann2413d.dll
+rem implib -a lib\opencv_gpu2413d        %OPENCV2_BIN_DIR%\opencv_gpu2413d.dll
+rem implib -a lib\opencv_highgui2413d    %OPENCV2_BIN_DIR%\opencv_highgui2413d.dll
+rem implib -a lib\opencv_imgproc2413d    %OPENCV2_BIN_DIR%\opencv_imgproc2413d.dll
+rem implib -a lib\opencv_legacy2413d     %OPENCV2_BIN_DIR%\opencv_legacy2413d.dll
+rem implib -a lib\opencv_ml2413d         %OPENCV2_BIN_DIR%\opencv_ml2413d.dll
+rem implib -a lib\opencv_nonfree2413d    %OPENCV2_BIN_DIR%\opencv_nonfree2413d.dll
+rem implib -a lib\opencv_objdetect2413d  %OPENCV2_BIN_DIR%\opencv_objdetect2413d.dll
+rem implib -a lib\opencv_ocl2413d        %OPENCV2_BIN_DIR%\opencv_ocl2413d.dll
+rem implib -a lib\opencv_photo2413d      %OPENCV2_BIN_DIR%\opencv_photo2413d.dll
+rem implib -a lib\opencv_stitching2413d  %OPENCV2_BIN_DIR%\opencv_stitching2413d.dll
+rem implib -a lib\opencv_superres2413d   %OPENCV2_BIN_DIR%\opencv_superres2413d.dll
+rem implib -a lib\opencv_video2413d      %OPENCV2_BIN_DIR%\opencv_video2413d.dll
+rem implib -a lib\opencv_videostab2413d  %OPENCV2_BIN_DIR%\opencv_videostab2413d.dll
