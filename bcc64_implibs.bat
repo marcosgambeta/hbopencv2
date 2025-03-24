715794890e913e9ab0f79e51b1908b7710e3ac46
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-03-24 12:12:52 -0300

    Add bcc64_implibs.bat

---
+++ bcc64_implibs.bat
@@ -0,0 +1,42 @@
+rem batch to import the OpenCV2 libraries using BCC64
+
+rem libraries
+mkexp lib\opencv_calib3d2413.a    %OPENCV2_BIN_DIR%\opencv_calib3d2413.dll
+mkexp lib\opencv_contrib2413.a    %OPENCV2_BIN_DIR%\opencv_contrib2413.dll
+mkexp lib\opencv_core2413.a       %OPENCV2_BIN_DIR%\opencv_core2413.dll
+mkexp lib\opencv_features2d2413.a %OPENCV2_BIN_DIR%\opencv_features2d2413.dll
+mkexp lib\opencv_ffmpeg2413.a     %OPENCV2_BIN_DIR%\opencv_ffmpeg2413.dll
+mkexp lib\opencv_flann2413.a      %OPENCV2_BIN_DIR%\opencv_flann2413.dll
+mkexp lib\opencv_gpu2413.a        %OPENCV2_BIN_DIR%\opencv_gpu2413.dll
+mkexp lib\opencv_highgui2413.a    %OPENCV2_BIN_DIR%\opencv_highgui2413.dll
+mkexp lib\opencv_imgproc2413.a    %OPENCV2_BIN_DIR%\opencv_imgproc2413.dll
+mkexp lib\opencv_legacy2413.a     %OPENCV2_BIN_DIR%\opencv_legacy2413.dll
+mkexp lib\opencv_ml2413.a         %OPENCV2_BIN_DIR%\opencv_ml2413.dll
+mkexp lib\opencv_nonfree2413.a    %OPENCV2_BIN_DIR%\opencv_nonfree2413.dll
+mkexp lib\opencv_objdetect2413.a  %OPENCV2_BIN_DIR%\opencv_objdetect2413.dll
+mkexp lib\opencv_ocl2413.a        %OPENCV2_BIN_DIR%\opencv_ocl2413.dll
+mkexp lib\opencv_photo2413.a      %OPENCV2_BIN_DIR%\opencv_photo2413.dll
+mkexp lib\opencv_stitching2413.a  %OPENCV2_BIN_DIR%\opencv_stitching2413.dll
+mkexp lib\opencv_superres2413.a   %OPENCV2_BIN_DIR%\opencv_superres2413.dll
+mkexp lib\opencv_video2413.a      %OPENCV2_BIN_DIR%\opencv_video2413.dll
+mkexp lib\opencv_videostab2413.a  %OPENCV2_BIN_DIR%\opencv_videostab2413.dll
+
+rem debug libraries
+rem mkexp lib\opencv_calib3d2413d.a    %OPENCV2_BIN_DIR%\opencv_calib3d2413d.dll
+rem mkexp lib\opencv_contrib2413d.a    %OPENCV2_BIN_DIR%\opencv_contrib2413d.dll
+rem mkexp lib\opencv_core2413d.a       %OPENCV2_BIN_DIR%\opencv_core2413d.dll
+rem mkexp lib\opencv_features2d2413d.a %OPENCV2_BIN_DIR%\opencv_features2d2413d.dll
+rem mkexp lib\opencv_flann2413d.a      %OPENCV2_BIN_DIR%\opencv_flann2413d.dll
+rem mkexp lib\opencv_gpu2413d.a        %OPENCV2_BIN_DIR%\opencv_gpu2413d.dll
+rem mkexp lib\opencv_highgui2413d.a    %OPENCV2_BIN_DIR%\opencv_highgui2413d.dll
+rem mkexp lib\opencv_imgproc2413d.a    %OPENCV2_BIN_DIR%\opencv_imgproc2413d.dll
+rem mkexp lib\opencv_legacy2413d.a     %OPENCV2_BIN_DIR%\opencv_legacy2413d.dll
+rem mkexp lib\opencv_ml2413d.a         %OPENCV2_BIN_DIR%\opencv_ml2413d.dll
+rem mkexp lib\opencv_nonfree2413d.a    %OPENCV2_BIN_DIR%\opencv_nonfree2413d.dll
+rem mkexp lib\opencv_objdetect2413d.a  %OPENCV2_BIN_DIR%\opencv_objdetect2413d.dll
+rem mkexp lib\opencv_ocl2413d.a        %OPENCV2_BIN_DIR%\opencv_ocl2413d.dll
+rem mkexp lib\opencv_photo2413d.a      %OPENCV2_BIN_DIR%\opencv_photo2413d.dll
+rem mkexp lib\opencv_stitching2413d.a  %OPENCV2_BIN_DIR%\opencv_stitching2413d.dll
+rem mkexp lib\opencv_superres2413d.a   %OPENCV2_BIN_DIR%\opencv_superres2413d.dll
+rem mkexp lib\opencv_video2413d.a      %OPENCV2_BIN_DIR%\opencv_video2413d.dll
+rem mkexp lib\opencv_videostab2413d.a  %OPENCV2_BIN_DIR%\opencv_videostab2413d.dll
