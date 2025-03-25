108b5a262c9f7f741d19bf11ac77d45cc4cfc318
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-03-25 18:50:33 -0300

    Update bcc32_makelib.bat

--- bcc32_makelib.bat
+++ bcc32_makelib.bat
@@ -4,10 +4,10 @@ rem remove the library
 del lib\hbopencv2.lib
 
 rem compile the source code
-bcc32 -O2 -d -c -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\core\core_c.cpp
-bcc32 -O2 -d -c -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\highgui\highgui_c.cpp
-bcc32 -O2 -d -c -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\imgproc\imgproc_c.cpp
-bcc32 -O2 -d -c -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\photo\photo_c.cpp
+bcc32 -O2 -d -c -Iinclude -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\core\core_c.cpp
+bcc32 -O2 -d -c -Iinclude -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\highgui\highgui_c.cpp
+bcc32 -O2 -d -c -Iinclude -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\imgproc\imgproc_c.cpp
+bcc32 -O2 -d -c -Iinclude -I%HB_PATH%\include -I%OPENCV2_INC_DIR% source\photo\photo_c.cpp
 
 rem create the library
 for %%1 in (*.obj) do tlib /P64 lib\hbopencv2.lib +%%1
