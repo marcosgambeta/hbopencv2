94bd71bbb9a7d6b2e4cc57df6c6a940d99761d74
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-03-26 05:48:00 -0300

    Update photo_c.cpp (macros)

--- source/photo/photo_c.cpp
+++ source/photo/photo_c.cpp
@@ -6,9 +6,10 @@
 
 #include <opencv2/photo/photo_c.h>
 #include <hbapi.h>
+#include "hbopencv2.hpp"
 
 // CVAPI(void) cvInpaint( const CvArr* src, const CvArr* inpaint_mask, CvArr* dst, double inpaintRange, int flags )
 HB_FUNC(CVINPAINT)
 {
-  cvInpaint((const CvArr *)hb_parptr(1), (const CvArr *)hb_parptr(2), (CvArr *)hb_parptr(3), hb_parnd(4), hb_parni(5));
+  cvInpaint(cv_cpar_CvArr(1), cv_cpar_CvArr(2), cv_par_CvArr(3), cv_par_double(4), cv_par_int(5));
 }
