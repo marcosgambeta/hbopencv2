c94cc5aa36ce52e9937d7ae676f67662f97f2ea2
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-03-27 07:52:40 -0300

    Update photo_c.cpp (parameters checking)

--- source/photo/photo_c.cpp
+++ source/photo/photo_c.cpp
@@ -6,10 +6,18 @@
 
 #include <opencv2/photo/photo_c.h>
 #include <hbapi.h>
+#include <hbapierr.h>
 #include "hbopencv2.hpp"
 
 // CVAPI(void) cvInpaint( const CvArr* src, const CvArr* inpaint_mask, CvArr* dst, double inpaintRange, int flags )
 HB_FUNC(CVINPAINT)
 {
-  cvInpaint(cv_cpar_CvArr(1), cv_cpar_CvArr(2), cv_par_CvArr(3), cv_par_double(4), cv_par_int(5));
+  if (hb_pcount() == 5 && HB_ISPOINTER(1) && HB_ISPOINTER(2) && HB_ISPOINTER(3) && HB_ISNUM(4) && HB_ISNUM(5))
+  {
+    cvInpaint(cv_cpar_CvArr(1), cv_cpar_CvArr(2), cv_par_CvArr(3), cv_par_double(4), cv_par_int(5));
+  }
+  else
+  {
+    hb_errRT_BASE(EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS);
+  }
 }
